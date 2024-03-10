e098b80c11a047af10f3769d2a8f87ac7fa0977b
Alessandro Lemser <lemser.alessandro@gmail.com>
2024-03-10 18:38:17 +0000

    Initial Commit

---
+++ Raw2Jpg/Raw2JpgApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raw2JpgApp.swift
+//  Raw2Jpg
+//
+//  Created by Alessandro Lemser on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Raw2JpgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
